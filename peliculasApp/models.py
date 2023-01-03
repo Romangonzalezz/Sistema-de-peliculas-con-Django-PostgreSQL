3e22d400b8a3104077a19de8aae0566fa5a191ed
Romangonzalezz <romanynaarut@gmail.com>
2023-01-03 08:57:12 -0300

    Creamos un modelo llamado Pelicula, y lo migramos a nuestra base de datos

--- peliculasApp/models.py
+++ peliculasApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+GENEROS_CHOICES = [ 
+    ('AC', 'Accion'),
+    ('AV', 'Aventura'),
+    ('AN', 'Animacion'),
+    ('DR', 'Drama'),
+    ('CF', 'Ciencia Ficcion'),
+    ('TE', 'Terror'),
+    ('MI', 'Misterio'),
+]
+
+STATUS_CHOICES = [ 
+    ('RA', 'Recientemente anidada'),
+    ('MV', 'Mas vista'),
+    ('TV', 'Mas valorada'),
+]
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=100)
+    sinopsis = models.TextField(max_length=1000)
+    anio = models.DateField()
+    imagen = models.ImageField(upload_to='peliculas')
+    genero = models.CharField(choices=GENEROS_CHOICES, max_length=2)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2)
+
+    def __str__(self):
+        return self.titulo
