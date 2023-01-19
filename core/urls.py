f7384bede1ad649c2b223280b6660953d295be2d
Romangonzalezz <romanynaarut@gmail.com>
2023-01-19 04:17:37 -0300

    Url redirect

--- core/urls.py
+++ core/urls.py
@@ -3,13 +3,13 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import re_path
 from django.views.generic.base import RedirectView
 from django.urls import reverse_lazy
+from peliculasApp.urls import PeliculaList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('', RedirectView.as_view(url=reverse_lazy('pelicula_list')), name='index'),
+    path('', RedirectView.as_view(url=reverse_lazy(PeliculaList)), name='index'),
     path('peliculas/', include('peliculasApp.urls', namespace = 'peliculas')),
     path('accounts/logout/', auth_views.LogoutView.as_view(),name='logout'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
