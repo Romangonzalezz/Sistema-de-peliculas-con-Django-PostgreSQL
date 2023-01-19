279d71267242bdf1d51c24598bf1e643e02934bf
Romangonzalezz <romanynaarut@gmail.com>
2023-01-19 04:02:15 -0300

    Url redirect

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,12 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from peliculasApp.views import PeliculaList
+from django.urls import re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^/$', RedirectView.as_view(url='pelicula_list', permanent=False), name='index'),
     path('peliculas/', include('peliculasApp.urls', namespace = 'peliculas')),
     path('accounts/logout/', auth_views.LogoutView.as_view(),name='logout'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
